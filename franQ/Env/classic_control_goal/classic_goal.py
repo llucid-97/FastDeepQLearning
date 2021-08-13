d83edff7196ae1431739965fc2b664b5a41c31f7
Gershom Agim <hexxagon6@gmail.com>
2021-08-13 08:33:38 +0100

    "[WIP] All classic envs are in working state except MountainCar. Needs  an exploration overhaul, but I won't add that.
     For envs with rare events [acrobot, lunar lander], it takes a long time to converge. Add a
     feature to address this (per?)
    
     I've also added skeletons for the GoalEnv variants of the classic
     control envs (all except MountainCarContinuous and Pendulum because
     their reward functions are not purely goal oriented. Needs a bit of
     thinking as to how best to do that, so let's solve the more obvious
     ones first, then work our way up

--- franQ/Env/classic_control_goal/classic_goal.py
+++ franQ/Env/classic_control_goal/classic_goal.py
@@ -21,6 +21,7 @@ class ClassicGoalEnv(wrappers.Wrapper):
         tasks = {
             "CartPole-v1": CartPoleGoalEnv,
             "Acrobot-v1": AcrobotGoalEnv,
+            "MountainCar-v0":MountainCarGoalEnv,
         }
         env = tasks[conf.name]()
         env = wrappers.ObsDictRenameKey(env, old_name="observation", new_name="obs_1d")
@@ -118,7 +119,7 @@ class CartPoleGoalEnv(wrappers.Wrapper):
     def compute_reward(self, achieved_goal: np.ndarray, desired_goal: np.ndarray, info) -> T.Tuple[float, bool]:
         # Deceptive reward: it is positive only when the goal is achieved
         if info.get("fail", False):
-            return -1, True
+            return -1.0, True
         if np.allclose(achieved_goal, desired_goal):
             return 1.0, False
         return 0.0, False
@@ -187,71 +188,88 @@ class AcrobotGoalEnv(wrappers.Wrapper):
         return reward, done
 
 
+class MountainCarGoalEnv(wrappers.Wrapper):
+    def __init__(self, ):
+        from gym.envs.classic_control.mountain_car import MountainCarEnv
+        import copy
+        env: MountainCarEnv = MountainCarEnv()
+        super().__init__(env)
+        # The achieved goal is determined by the current state
+        # here, it is a special where they are equal
+        # import math
+        # env.theta_threshold_radians = 90 * 2 * math.pi / 360
+        max_steps: T.Optional[int] = 500
+        self.observation_space = spaces.Dict({
+            "observation": env.observation_space,
+            "achieved_goal": env.observation_space,
+            "desired_goal": env.observation_space,
+        })
+
+        self.desired_goal = self.observation_space["desired_goal"].sample()
+        self.desired_goal[0] = env.goal_position
+        self.desired_goal[1] = env.goal_velocity
+        self._max_episode_steps = float("inf") if max_steps in (None, 0) else max_steps
+        self.reset()
+
+    def _get_obs(self) -> OrderedDict:
+        """
+        Helper to create the observation.
+
+        :return: (OrderedDict<int or ndarray>)
+        """
+
+        return OrderedDict([
+            ("observation", self.obs.copy()),
+            ("achieved_goal", self.obs.copy()),
+            ("desired_goal", self.desired_goal.copy()),
+        ])
+
+    def reset(self) -> OrderedDict:
+        self.current_step = 0
+        self.obs = self.env.reset()
+        return self._get_obs()
+
+    def step(self, action: T.Union[np.ndarray, int]):
+        self.obs, _, _, info = self.env.step(action)
+        obs = self._get_obs()
+        reward, done = self.compute_reward(obs["achieved_goal"], obs["desired_goal"], info)
+        self.current_step += 1
+        # Episode terminate when we reached the goal or the max number of steps
+        if self.current_step >= self._max_episode_steps:
+            done = True
+            info['TimeLimit.truncated'] = True
+        return obs, reward, done, info
+
+    def compute_reward(self, achieved_goal: np.ndarray, desired_goal: np.ndarray, info) -> T.Tuple[float, bool]:
+        done = np.all(achieved_goal >= desired_goal)
+        reward = 50.0 if done else -1.0
+        return reward, done
+
+
 if __name__ == '__main__':
 
     def main():
-        env = AcrobotGoalEnv()
-        for i_episode in range(5):
-            obs = env.reset()
-            min_ag = float("inf")
-            max_ag = -float("inf")
-            for t in range(int(1e6)):
-                env.render()
-                action = env.action_space.sample()
-                obs, reward, done, info = env.step(action)
-                ag = obs['achieved_goal']
-                min_ag = min(ag, min_ag)
-                max_ag = max(ag, max_ag)
-                from franQ.common_utils import numpy_set_print_decimal_places
-                numpy_set_print_decimal_places(2)
-                print(f"{ag}\t{min_ag}\t{max_ag}\t{obs['observation']}")
-                import time
-                time.sleep(0.05)
-                if done:
-                    print("Episode finished after {} timesteps".format(t + 1))
-                    break
-        env.close()
-
-
-    main()
-
-if __name__ == '__main__':
-
-    def main():
-        def sigmoid(x):
-            return 1.0 / (1.0 + np.exp(-x))
-
-        env = CartPoleGoalEnv()
-
-        P, I, D = 0.1, 0.01, 0.5
-
-        for i_episode in range(5):
-            obs = env.reset()
-            desired_state = obs["desired_goal"]
-
-            state = obs["achieved_goal"]
-            integral = 0
-            prev_error = 0
-            for t in range(500):
-                env.render()
-                error = state - desired_state
-
-                integral += error
-                derivative = error - prev_error
-                prev_error = error
-
-                pid = np.dot(P * error + I * integral + D * derivative, env.goal_mask)
-                action = sigmoid(pid)
-                action = np.round(action).astype(np.int32)
-
-                obs, reward, done, info = env.step(action)
-                state = obs["achieved_goal"]
-                desired_state = obs["desired_goal"]
-
-                if done:
-                    print("Episode finished after {} timesteps".format(t + 1))
-                    break
-        env.close()
+        for Env_T in (MountainCarGoalEnv, CartPoleGoalEnv, AcrobotGoalEnv):
+            env = Env_T()
+            for i_episode in range(2):
+                obs = env.reset()
+                for t in range(int(1e6)):
+                    env.render()
+                    action = env.action_space.sample()
+                    obs, reward, done, info = env.step(action)
+                    assert isinstance(obs, dict)
+                    assert "observation" in obs
+                    assert "achieved_goal" in obs
+                    assert "desired_goal" in obs
+                    for k in obs:
+                        if isinstance(env.observation_space.spaces[k], gym.spaces.Box):
+                            assert obs[k].shape == env.observation_space.spaces[k].shape
+                            assert np.all(obs[k] <= env.observation_space.spaces[k].high)
+                            assert np.all(obs[k] >= env.observation_space.spaces[k].low)
+                    if done:
+                        print("Episode finished after {} timesteps".format(t + 1))
+                        break
+            env.close()
 
 
     main()
